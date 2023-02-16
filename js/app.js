cd820bce485127e0a57f7b5480bddd982d2a487b
Stina <stina.kalnina@gmail.com>
2023-02-16 18:20:08 +0200

    navigate throu sections

--- js/app.js
+++ js/app.js
@@ -1,37 +1,31 @@
 const ul = document.getElementById('list')
-ul.innerHTML += `<li><a href="#sectionLink1">Item1 </a></li>`
-ul.innerHTML += `<li><a href="#sectionLink2">Item2 </a></li>`
-ul.innerHTML += `<li><a href="#sectionLink3">Item3 </a></li>`
+ul.innerHTML += `<li><a href="#section">Section </a></li>`
+ul.innerHTML += `<li><a href="#section1">Section 2 </a></li>`
+ul.innerHTML += `<li><a href="#section2">Section 3 </a></li>`
+ul.innerHTML += `<li><a href="#section3">Section 4 </a></li>`
 
-document.addEventListener('DOMContentLoaded', () => {
-  const listOfLinks = document.querySelectorAll("a[href^='#sectionLink")
-  const sections = document.querySelectorAll('div')
+const navigation = document.querySelectorAll('header nav a')
+const sections = document.querySelectorAll('sections h2')
 
-  listOfLinks.forEach(function (link) {
-    sections.forEach(section => {
-      link.addEventListener('click', () => {
-        listOfLinks.forEach(link => {
-          sections.forEach(section => {
-            if (
-              link.classList.contains('highlighted') &&
-              section.classList.contains('visited')
-            ) {
-              link.classList.remove('highlighted') &&
-                section.classList.remove('visited')
-            }
-          })
-        })
+navigation.forEach(nav => {
+  nav.addEventListener('click', smoothScroll)
+})
 
-        link.classList.add('highlighted')
-        section.classList.add('visited')
-        let ref = link.href.split('#sectionLink')
-        ref = '#section' + ref[1]
-        window.scroll({
-          behavior: 'smooth',
-          left: 0,
-          top: document.querySelector(ref).offsetTop
-        })
-      })
+navigation.forEach(function (nav) {
+  nav.addEventListener('click', () => {
+    navigation.forEach(nav => {
+      if (nav.classList.contains('highlighted')) {
+        nav.classList.remove('highlighted')
+      }
     })
+    nav.classList.add('highlighted')
   })
 })
+
+function smoothScroll (event) {
+  event.preventDefault()
+  const href = this.getAttribute('href')
+  document.querySelector(href).scrollIntoView({
+    behavior: 'smooth'
+  })
+}
